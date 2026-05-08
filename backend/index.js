2a0fa3e40b6e277c93981bfe588954675bd9b995
Samuel Yeshambel <samuaelyeshambel@gmail.com>
2026-05-09 00:42:48 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,5 +15,5 @@ app.get('/api/test', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on https://sis-hackathon-2026.vercel.app:${PORT}`);
 });
